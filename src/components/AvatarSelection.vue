31972b93486524ac6e3f4bfa3b1bc506682d4454
Christoph Keller <christoph.keller@bambit.ch>
2023-08-26 22:15:22 +0200

    fix: Loader backdrop fixed.

--- src/components/AvatarSelection.vue
+++ src/components/AvatarSelection.vue
@@ -149,15 +149,15 @@ export default {
   &:after {
     content: "";
     background-color: rgba(0, 0, 0, 0.5);
-    position: absolute;
+    position: fixed;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
     z-index: 1;
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-left: -15px;
-    margin-right: -15px;
   }
 }
 .avatar-image-container {
